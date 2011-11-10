91825df871be9a4cad7cb8b142a5cacf08464737
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2011-11-10 03:31:50 -0600

    autogenerate plt file

--- results.py
+++ results.py
@@ -204,6 +204,53 @@ for n in nset:
 			f.write("\n")
 			f.write("\n")
 
+with open("graphs/cannon-dns.plt", "w") as f:
+	f.write("""#!/usr/bin/gnuplot -persist
+
+#set title "101-303 with e=0.2, clTh=0.1"
+set notitle
+set auto x
+set auto y
+#set xtics 10 
+#set ytics 5
+#set style data lines
+#set style fill solid border -1
+#set style histogram cluster gap 5
+set boxwidth 0.9
+
+#set log y
+
+set ylabel "Runtime (s)"
+set xlabel "(processors, cores)"
+
+
+set grid noxtics nomxtics ytics mytics 
+#unset key
+set key right top
+
+
+#set lmargin 4
+#set rmargin 3.5
+
+set datafile separator ":"
+
+set bmargin 0
+#set lmargin 3
+set tmargin 0
+set rmargin 1
+
+set terminal postscript enhanced color""")
+	for n in nset:
+		for k in kset:
+			name = "cannon-dns-%d-%d" % (n,k)
+			f.write("""set output '| ps2pdf - ../images/%s.pdf'
+
+plot '%s.txt' using 2:xticlabels(1) with lines lw 3 title "cannon", \\
+     '%s.txt' using 3:xticlabels(1) with lines lw 3 title "cannon strassen", \\
+     '%s.txt' using 4:xticlabels(1) with lines lw 3 title "dns", \\
+     '%s.txt' using 5:xticlabels(1) with lines lw 3 title "dns strassen"
+""" % (name, name, name, name, name))
+
 # SEP=":"
 # for prog in sorted(progset):
 # 	print prog,
